5c5f332bd276105246b0776ac6ff228de2468893
Lyubomir Popov <lhpopov@yahoo.com>
2019-02-27 14:32:58 +0200

    Order now displays items in basket and their total value as well as their names.

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import StorePicker from './StorePicker';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from './../sample-fishes.js';
+import Fish from './Fish';
 
 class App extends React.Component {
     state = {
@@ -15,16 +17,30 @@ class App extends React.Component {
         fishes[`fish${Date.now()}`] = fish;
 
         this.setState({ fishes });
-    }
+    };
+
+    loadSampleFishes = () => {
+        this.setState({ fishes: sampleFishes });
+    };
+
+    addToOrder = (key) =>{
+        const order = { ...this.state.order} ;
+        
+        order[key] = order[key] + 1 || 1;
+        this.setState({ order });
+    };
 
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh daily"/>
+                    <ul>
+                        {Object.keys(this.state.fishes).map(key => <Fish index={key} key={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)}
+                    </ul>
                 </div>
-                <Order />
-                <Inventory addFish={this.addFish} />
+                <Order fishes={this.state.fishes} order={this.state.order}/>
+                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
             </div>
         )
     }
